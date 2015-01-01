8f37855140fdebaf3998b449fb0d4d2f5c6f5fff
Garry Gardama <katenzo@gmail.com>
2015-01-01 21:57:30 +0700

    Update gradle, step def

--- app/src/androidTest/java/com/example/garry/timun/MainActivityStepDefs.java
+++ app/src/androidTest/java/com/example/garry/timun/MainActivityStepDefs.java
@@ -5,6 +5,7 @@ import android.test.ActivityInstrumentationTestCase2;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -17,17 +18,23 @@ import cucumber.api.junit.Cucumber;
 
 public class MainActivityStepDefs extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    public MainActivityStepDefs(Class<MainActivity> activityClass) {
-        super(activityClass);
+    public MainActivityStepDefs() {
+        super(MainActivity.class);
     }
 
     @Given("^I am launching the application$")
-    public void I_am_launching_the_application() throws Exception { }
+    public void I_am_launching_the_application() throws Exception {
+        throw new PendingException();
+    }
 
     @When("^I submit a text$")
-    public void I_submit_a_text() throws Exception { }
+    public void I_submit_a_text() throws Exception {
+        throw new PendingException();
+    }
 
     @Then("^I receive the same text on the next page$")
-    public void I_receive_the_same_text_on_the_next_page() throws Exception {  }
+    public void I_receive_the_same_text_on_the_next_page() throws Exception {
+        throw new PendingException();
+    }
 
 }
